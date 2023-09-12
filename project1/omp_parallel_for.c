414c1d430f80ade179a966af759dd2cbc165b892
JooKai <bsubs02@gmail.com>
2023-09-12 23:01:32 +0800

    Update omp_parallel_for.c

--- project1/omp_parallel_for.c
+++ project1/omp_parallel_for.c
@@ -13,21 +13,18 @@ void parallelFor(Fish* fishArray){
         #pragma omp parallel
         {
             #pragma omp for reduction(max: maxDiff)
-            // Loops through fish array and finds maxDiff in the current round
             for(int j = 0; j < NUMFISH; j++) {
                 double dist = fishArray[j].euclDist;
                 if(dist > maxDiff) {maxDiff = dist;}
             }
 
             #pragma omp for
-            // Loops through fish array and performs eat & swim operations
             for(int j = 0; j < NUMFISH; j++){
                 eat(&fishArray[j], maxDiff, i);
                 swim(&fishArray[j]);
             }
 
             #pragma omp for reduction(+ : sumOfProduct, sumOfDistance)
-            // Loops through fish array and accumulates variables for barycentre
             for(int j = 0; j < NUMFISH; j++){
                 sumOfProduct += fishArray[j].euclDist * fishArray[j].weight_p;
                 sumOfDistance += fishArray[j].euclDist;
