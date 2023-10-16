0559126209d852d3e2ab6fbf58f9ab1dd5934195
JooKai <bsubs02@gmail.com>
2023-10-16 16:00:46 +0800

    Created experiment 6

---
+++ project2/experiment5.sh
@@ -0,0 +1,14 @@
+#!/bin/sh 
+#SBATCH --account=courses0101
+#SBATCH --partition=debug
+#SBATCH --ntasks=4
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=00:60:00
+#SBATCH --exclusive
+#SBATCH --mem-per-cpu=32G
+#module load  openmpi/4.0.5
+
+export OMP_NUM_THREADS=4
+echo "Num Nodes: 4"
+mpicc -fopenmp -o experiment5 experiment5.c fish.c -lm
+srun ./experiment5
